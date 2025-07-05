2a5bd17c0b6837907616192de7176a324136d00d
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-07-05 18:44:01 +0600

    Borrow Api Added in store

--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -1,18 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { bookApi } from '../api/bookApi';
+import { borrowApi } from '../api/borrowApi';
 
 export const store = configureStore({
   reducer: {
-    [bookApi.reducerPath]: bookApi.reducer
+    [bookApi.reducerPath]: bookApi.reducer,
+    [borrowApi.reducerPath]: borrowApi.reducer
   },
    middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(bookApi.middleware)
+      .concat(borrowApi.middleware)
       
 
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
